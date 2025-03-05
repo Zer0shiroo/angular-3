ae2a58de94a9422ca4da11d46e25f2397fb490b6
El Chambeador <z3r0sh1r0@gmail.com>
2025-03-05 17:28:34 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto3';
+  paises: any; // Atributo para almacenar los países
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    // Hacemos la solicitud HTTP GET a la API de COVID-19
+    this.http.get("https://api.covid19api.com/summary")
+      .subscribe(
+        resultado => {
+          this.paises = resultado;  // Guardamos la respuesta en 'paises'
+        }
+      );
+  }
 }
